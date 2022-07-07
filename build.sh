fd61b9516c55aa9374cc5a351be52331821726b9
sharpenedblade <sharpenedblade@proton.me>
2022-07-07 15:59:26 -0700

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eu -o pipefail
+
+FEDORA_KERNEL_VERSION=5.18.6-200.fc36
+REPO_PWD=$(pwd)
+
+### Dependencies
+dnf install -y fedpkg fedora-packager rpmdevtools ncurses-devel pesign git libkcapi libkcapi-devel libkcapi-static libkcapi-tools curl dwarves libbpf zip
+
+## Set home build directory
+rpmdev-setuptree
+
+## Install the kernel source and finish installing dependencies
+cd /root/rpmbuild/SOURCES
+koji download-build --arch=src kernel-${FEDORA_KERNEL_VERSION}
+rpm -Uvh kernel-${FEDORA_KERNEL_VERSION}.src.rpm
+
+cd /root/rpmbuild/SPECS
+dnf -y builddep kernel.spec
+
+### Create patch file with custom drivers
+FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} "/repo"/patch_driver.sh
+
+### Apply patches
+mkdir -p "/repo"/patches
+while IFS= read -r file
+do
+  "/repo"/patch_kernel.sh "$file"
+done < <(find "/repo"/patches -type f -name "*.patch" | sort)
+
+echo "CONFIG_APPLE_BCE=m" >> "/root/rpmbuild/SOURCES/kernel-local"
+echo "CONFIG_APPLE_IBRIDGE=m" >> "/root/rpmbuild/SOURCES/kernel-local"
+
+### Change buildid to mbp
+sed -i "s/# define buildid.*/%define buildid .t2-fc36/" "/root/rpmbuild"/SPECS/kernel.spec
+
+### Build non-debug rpms
+cd "/root/rpmbuild"/SPECS
+rpmbuild -bb --with baseonly --without debug --without debuginfo --target=x86_64 kernel.spec
+
+### Copy artifacts to shared volume
+cd "/repo"
+mkdir -p ./output
+mkdir -p ./output/RPMS
+cp -rfv /root/rpmbuild/RPMS/* ./output/RPMS/
---
+++ patch_driver.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+### Apple T2 drivers commit hashes
+APPLE_SMC_DRIVER_GIT_URL=https://github.com/AdityaGarg8/linux-t2-patches
+APPLE_SMC_REPO_NAME=linux-t2-patches
+APPLE_SMC_DRIVER_BRANCH_NAME=main
+APPLE_SMC_DRIVER_COMMIT_HASH=5de541ee8cd4d8cf54a25d35c9eb95c6fca0603e
+
+# TMP_DIR=~/temp_dir
+TMP_DIR=/tmp/temp_dir
+REPO_PWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+PATCHES_DIR=${PATCHES_DIR:-$REPO_PWD/patches}
+
+mkdir -p "${TMP_DIR}"
+cd "${TMP_DIR}" || exit
+
+mkdir -p "${PATCHES_DIR}"
+
+### AppleSMC and BT aunali fixes
+git clone --single-branch --branch ${APPLE_SMC_DRIVER_BRANCH_NAME} ${APPLE_SMC_DRIVER_GIT_URL}
+cd "${APPLE_SMC_REPO_NAME}" || exit
+git checkout ${APPLE_SMC_DRIVER_COMMIT_HASH}
+cd ..
+while IFS= read -r file; do
+  echo "adding ${file}"
+  cp -rfv "${file}" "${PATCHES_DIR}"/"${file##*/}"
+done < <(find "${APPLE_SMC_REPO_NAME}" -type f -name "*.patch" | sort)
+
+rm -rf "${TMP_DIR}"
