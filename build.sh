fb7119dcb1e6a738ca085f9efef32f72ee95790b
example <example@example.com>
2022-07-13 15:19:11 -0700

    Use only one build script.

--- build.sh
+++ build.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
-set -eu -o pipefail
 
 FEDORA_KERNEL_VERSION=5.18.6-200.fc36
-REPO_PWD=$(pwd)
+PATCHES_GIT=https://github.com/AdityaGarg8/linux-t2-patches
+PATCHES_COMMIT=5de541ee8cd4d8cf54a25d35c9eb95c6fca0603e
 
-### Dependencies
-dnf install -y fedpkg fedora-packager rpmdevtools ncurses-devel pesign git libkcapi libkcapi-devel libkcapi-static libkcapi-tools curl dwarves libbpf zip
+# Dependencies
+dnf install -y fedora-packager git curl pesign ncurses-devel libkcapi libkcapi-devel libkcapi-static libkcapi-tools libbpf fedpkg rpmdevtools dwarves
 
-## Set home build directory
+# Set home build directory
 rpmdev-setuptree
 
-## Install the kernel source and finish installing dependencies
+# Get kernel source and install dependencies
 cd /root/rpmbuild/SOURCES
 koji download-build --arch=src kernel-${FEDORA_KERNEL_VERSION}
 rpm -Uvh kernel-${FEDORA_KERNEL_VERSION}.src.rpm
-
-cd /root/rpmbuild/SPECS
+cd /root/rpmbuild/SPECS 
 dnf -y builddep kernel.spec
 
-### Create patch file with custom drivers
-FEDORA_KERNEL_VERSION=${FEDORA_KERNEL_VERSION} "/repo"/patch_driver.sh
+# Download patches
+mkdir /tmp/download && cd /tmp/download 
+git clone --single-branch --branch main ${PATCHES_GIT}
+cd *
+git checkout ${PATCHES_COMMIT}
+for f in *.patch; do mv "$f" "$f.t2"; done
+cp *.patch.t2 /root/rpmbuild/SOURCES/
 
-### Apply patches
-mkdir -p "/repo"/patches
-while IFS= read -r file
-do
-  "/repo"/patch_kernel.sh "$file"
-done < <(find "/repo"/patches -type f -name "*.patch" | sort)
-
+# Apply patches
+cd /root/rpmbuild/SPECS
 echo "CONFIG_APPLE_BCE=m" >> "/root/rpmbuild/SOURCES/kernel-local"
 echo "CONFIG_APPLE_IBRIDGE=m" >> "/root/rpmbuild/SOURCES/kernel-local"
-
-### Change buildid to mbp
 sed -i "s/# define buildid.*/%define buildid .t2/" "/root/rpmbuild"/SPECS/kernel.spec
 
-### Build non-debug rpms
+for i in /root/rpmbuild/SOURCES/*.patch.t2; do
+    PATCH_ID=$(($(grep ^Patch "/root/rpmbuild/SPECS/kernel.spec" | tail -n2 | head -n1 | awk '{ print $1 }' | sed s/Patch// | sed s/://) + 1 ))
+    sed -i "s@# END OF PATCH DEFINITIONS@Patch$PATCH_ID: $i\n\n# END OF PATCH DEFINITIONS@g" /root/rpmbuild/SPECS/kernel.spec
+done;
+
+# Build non-debug rpms
 cd "/root/rpmbuild"/SPECS
 rpmbuild -bb --with baseonly --without debug --without debuginfo --target=x86_64 kernel.spec
 
-### Copy artifacts to shared volume
+# Copy artifacts to shared volume
 cd "/repo"
 mkdir -p ./output
 mkdir -p ./output/RPMS
