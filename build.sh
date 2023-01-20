867248c143e015d24d429d9a8cda412f56fdc58f
sharpenedblade <sharpenedblade@proton.me>
2023-01-19 16:14:34 -0800

    Rebuild with new signing key.

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ git checkout ${PATCHES_COMMIT}
 echo "=====PREPARING SOURCES====="
 cd ~/rpmbuild/SPECS
 sed -i 's/# define buildid .local/%define buildid .t2/g' kernel.spec
-# sed -i 's/%define pkgrelease 200/%define pkgrelease 201/' kernel.spec
+sed -i 's/%define pkgrelease 200/%define pkgrelease 201/' kernel.spec
 mv -f /repo/python-blivet.spec /repo/rpmbuild/SPECS/python-blivet.spec
 mv /repo/0002-add-t2-support.patch /root/rpmbuild/SOURCES/0002-add-t2-support.patch
 cat /tmp/download/*/extra_config > /root/rpmbuild/SOURCES/kernel-local
