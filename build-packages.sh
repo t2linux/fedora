ff9d91d13c2d46751e000c305d8fb27017df3818
sharpenedblade <sharpenedblade@proton.me>
2023-07-24 16:14:25 +0530

    feat: Do not download dependencies in build scripts

--- build-packages.sh
+++ build-packages.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/bash
 source /repo/util.sh
 
-# Install dependencies
-dnf install -y --quiet koji git rpmdevtools rpm-sign mock
-
-# Optimize mock
-echo 'config_opts["plugin_conf"]["package_state_enable"] = False
-config_opts["macros"]["%_smp_mflags"] = "-j8"
-config_opts["plugin_conf"]["ccache_opts"]["compress"] = True
-config_opts["plugin_conf"]["root_cache_opts"]["compress_program"] = "zstd"
-config_opts["plugin_conf"]["root_cache_opts"]["extension"] = ".zst"
-config_opts["plugin_conf"]["hw_info_enable"] = False' > /etc/mock/site-defaults.cfg
-
 cd /repo
 
 if [ "$PACKAGE" == "kernel" ]; then
