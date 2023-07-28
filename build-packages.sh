27e1e6ea510cb37bc07bb4261b75d63a0742944f
Sharpened Blade <sharpenedblade@proton.me>
2023-07-28 15:31:49 +0530

    Accept package as an argument to build-packages.sh
    The build script will not build the package specified in $PACKAGE.
    The script will only build the first specified package, any other
    arguments will be ignored.
    
    Merges https://github.com/t2linux/t2linux-fedora-kernel/pull/11

--- build-packages.sh
+++ build-packages.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 source /repo/util.sh
 
-if [[ -z "$PACKAGE" ]]; then
-    PACKAGE=""
+if [[ -z "$1" ]]; then
+    package=""
     echo "Available packages: t2linux-config, t2linux-repo, t2linux-config, or kernel"
-    read -p "Name of package to build: " PACKAGE
+    read -p "Name of package to build: " package
+else
+    package="$1"
 fi
 
 cd /repo
 
-if [ "$PACKAGE" == "kernel" ]; then
+if [ "$package" == "kernel" ]; then
     /repo/kernel/kernel.sh
 fi
 
-cd /repo/$PACKAGE
-build_package $PACKAGE.spec
+cd /repo/$package
+build_package $package.spec
