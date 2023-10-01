5b3bddda88e3fc32b7c8aa923b4875d4060891e6
sharpenedblade <sharpenedblade@proton.me>
2023-10-01 16:04:51 -0700

    Use submodules

--- kernel/kernel.sh
+++ kernel/kernel.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 FEDORA_KERNEL_VERSION=6.5.5-200.fc38
-PATCHES_COMMIT=3aeff9385079f5f362a5447cdb9f4e2b48962c92
-
 cd /repo/kernel
 koji download-build --quiet --arch=src "kernel-${FEDORA_KERNEL_VERSION}"
 rpmdev-extract -q "kernel-${FEDORA_KERNEL_VERSION}".src.rpm
@@ -16,9 +14,7 @@ sed -i 's/# define buildid .local/%define buildid .t2/g' kernel.spec
 # Disable debug kernels
 sed -i "/%define with_debug /c %define with_debug 0" kernel.spec
 
-git clone --quiet --single-branch --branch main https://github.com/t2linux/linux-t2-patches patches && cd patches
-git checkout --quiet ${PATCHES_COMMIT}
-cd ..
+cat linux-t2-patches/extra_config > kernel-local
+cat linux-t2-patches/*.patch > linux-kernel-test.patch
 
-cat patches/extra_config > kernel-local
-cat patches/*.patch > linux-kernel-test.patch
+cd ..
