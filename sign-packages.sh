0a5c1f4e786998f10c7c8fdbea1826eb36daf041
sharpenedblade <sharpenedblade@proton.me>
2023-10-02 15:25:24 -0700

    Fix deploy workflow

--- sign-packages.sh
+++ sign-packages.sh
@@ -2,5 +2,5 @@
 cd /repo/_output || exit 2
 
 echo "$RPM_SIGNING_PRIVATE_KEY_B64" | base64 -d | gpg --import
-rpm --addsign ./*.rpm
 echo -e "%_signature gpg\n%_gpg_name T2Linux Fedora" > ~/.rpmmacros
+rpm --addsign ./*.rpm
