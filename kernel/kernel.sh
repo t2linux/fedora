d5d061078a93cf8f6786ca28464b753612f8b0ea
sharpenedblade <sharpenedblade@proton.me>
2024-06-17 16:56:18 -0700

    Build debug kernels

--- kernel/kernel.sh
+++ kernel/kernel.sh
@@ -9,14 +9,11 @@ rpmdev-extract -q "kernel-$KERNEL_VERSION.src.rpm"
 mv -n "kernel-$KERNEL_VERSION.src"/* .
 rm -r "kernel-$KERNEL_VERSION.src.rpm" "kernel-$KERNEL_VERSION.src"
 
-# Fedora devs are against merging kernel-local for all architectures when keys are not properly specified, so we have to patch it in.
-sed -i "s@for i in %{all_arch_configs}@for i in *.config@g" "kernel.spec"
-
 # Set buildid to .t2
 sed -i 's/# define buildid .local/%define buildid .t2/g' "kernel.spec"
 
-# Disable debug kernels
-sed -i "/%define with_debug /c %define with_debug 0" "kernel.spec"
+# Enable debug kernels
+sed -i "/%define with_debug /c %define with_debug 1" "kernel.spec"
 
 # Add our patches
 sed -i "/Patch1:/a Patch2: t2linux-combined.patch" "kernel.spec"
