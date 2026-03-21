613d83eff01a0345520554adc41dbe3944a9c56a
sharpenedblade <sharpenedblade@proton.me>
2026-03-21 15:50:59 -0700

    kerrnel: Fix copr env vars

--- kernel/copr-custom-build-script.sh
+++ kernel/copr-custom-build-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 git clone --recurse-submodules https://github.com/t2linux/fedora t2-fedora
-cd t2-fedora/kernel
-export package_builddir="$PWD"
+cd t2-fedora/kernel || exit
+export sourcedir="$COPR_RESULTDIR"
 ./kernel.sh
